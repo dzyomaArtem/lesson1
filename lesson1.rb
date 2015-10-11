08329178a21ffe6c4ceb201db5b2e1c8726e8e06
dzyomaArtem <dz.art.23@yandex.ua>
2015-10-12 00:27:28 +0300

    homework_v1

--- lesson1.rb
+++ lesson1.rb
@@ -1,14 +1,33 @@
 require 'date'
 class Lesson1
   def sum(val = 0)
-    # TODO
+    p "Сумма введених чисел: #{val.split('').map{ |i| i.to_i }.inject{ |val,sum| val + sum }.to_s}"
   end
 
   def age(birthday)
-    # TODO
+	timeNow = Time.now
+	#birthday = Time.mktime(1995, 7, 23)
+	secondValue = timeNow - birthday
+	second = secondValue.to_i
+	#years = timeNow.year - birthday.year
+	p "Я живу #{second/(3600*365*24)} лет или #{second/(3600*24)} дней или #{second/3600} часов или #{second/60} минут или #{second} секунд"
   end
 
   def name
-    # TODO
+    p "Введите фамилию:"
+    surname = gets.chomp
+    p "Введите имя:"
+    name = gets.chomp
+    p "Введите отчество:"
+    middlename = gets.chomp 
+    p "Hello #{surname.capitalize.to_s + " " + name.capitalize.to_s + " " + middlename.capitalize.to_s}!"
   end
 end
+
+l1 = Lesson1.new
+#numbers = gets.chomp
+#l1.sum(numbers)
+#--------------
+dateOfBirthday = Time.mktime(1995, 11, 4)
+l1.age(dateOfBirthday)
+#l1.name
--- my_array.rb
+++ my_array.rb
@@ -1,48 +1,49 @@
 class MyArray
   attr_accessor :array
 
-  def initialize(arr = [])
+  def initialize(arr = [5, 7, 3, 8, 1, 8, 10])
     @array = arr
+    p "Начальный массив: #{array.join(", ")}"
   end
 
   def size
-    # TODO
+    p "Размер массива: #{array.length}"
   end
 
   def reverse
-    # TODO
+    p "Перевернутый массив: #{array.reverse}"
   end
 
   def max
-    # TODO
+    p "Наибольший элемент массива: #{array.max}"
   end
 
   def min
-    # TODO
+    p "Наименьший элемент массива: #{array.min}"
   end
 
   def desc
-    # TODO
+    p "Отсортированный массив (по убыванию): #{array.sort{|a, b| b <=> a}}" 
   end
 
   def asc
-    # TODO
+    p "Отсортированный массив (по возрастанию): #{array.sort}" 
   end
 
   def odd
-    # TODO
+    p "Нечетные числа массива: #{array.select {|number| number % 2 == 1}}"
   end
 
   def multiple_to_three
-    # TODO
+    p "Числа массива кратные 3: #{array.select {|number| number % 3 == 0}}"
   end
 
   def uniq
-    # TODO
+    p "Уникальные числа массива: #{array.uniq}"
   end
 
   def devide_on_ten
-    # TODO
+    p "Элементы, которые розделены на 10 с значением после запятой: #{array.map { |i| i/10.0 }}"
   end
 
   def chars
@@ -50,7 +51,12 @@ class MyArray
   end
 
   def switch
-    # TODO
+    newArr = array.dup
+    min = array.index(array.min)
+    max = array.index(array.max)
+    
+    newArr[min], newArr[max] = array.max, array.min
+    p "Переставленные местами мин. и максимальный элементы массива: #{newArr}"
   end
 
   def before_min
@@ -61,3 +67,29 @@ class MyArray
     # TODO
   end
 end
+
+#myArray = MyArray.new
+
+
+#1
+#myArray.size
+#2
+#myArray.reverse
+#3
+#myArray.max
+#4
+#myArray.min
+#5
+#myArray.desc
+#6
+#myArray.asc
+#7
+#myArray.odd
+#8
+#myArray.multiple_to_three
+#9
+#myArray.uniq
+#10
+#myArray.devide_on_ten
+#11
+#myArray.switch
