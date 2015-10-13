5416e0a29b98fb396e9a7c60fe27159e33fc6e3c
dzyomaArtem <dz.art.23@yandex.ua>
2015-10-13 20:41:58 +0300

    homework_v4

--- lesson1.rb
+++ lesson1.rb
@@ -1,25 +1,26 @@
 require 'date'
 class Lesson1
   def sum(val = 0)
-    val.split('').map{ |i| i.to_i }.inject{ |val,sum| val + sum }.to_s
+    val.to_s.split('').inject(0) { |a, e| a + e }.to_s
   end
 
   def age(birthday)
-	if birthday 
-	  days = (Date.parse(Time.now.to_s) - Date.parse(birthday)).to_i 
-	  years = days / 365 
-	  hours = days * 24 
-	  min = hours * 60 
-	  sec = min * 60 
-	  "Я живу #{years} лет или #{days} дней или #{hours} часов или #{min} минут или #{sec} секунд"
-	else 'Неправильный формат даты'
-	end
+    if birthday
+      days = (Date.parse(Time.now.to_s) - Date.parse(birthday)).to_i
+      years = days / 365
+      hours = days * 24
+      min = hours * 60
+      sec = min * 60
+      "Я живу #{years} лет или #{days} дней или #{hours} часов
+         или #{min} минут или #{sec} секунд"
+    else 'Неправильный формат даты'
+    end
   end
 
   def name
     surname = gets.chomp
     name = gets.chomp
-    middlename = gets.chomp 
+    middlename = gets.chomp
     "Hello #{name} #{surname} #{middlename}!"
   end
 end
--- my_array.rb
+++ my_array.rb
@@ -22,7 +22,7 @@ class MyArray
   end
 
   def desc
-    @array.sort{|a, b| b <=> a}
+    @array.sort { |a, b| b <=> a }
   end
 
   def asc
@@ -30,11 +30,11 @@ class MyArray
   end
 
   def odd
-    @array.select {|number| number % 2 == 1}
+    @array.select { |number| number % 2 > 0 }
   end
 
   def multiple_to_three
-    @array.select {|number| number % 3 == 0}
+    @array.select { |number| number % 3 == 0 }
   end
 
   def uniq
@@ -42,27 +42,23 @@ class MyArray
   end
 
   def devide_on_ten
-    @array.map { |i| i/10.0 }
+    @array.map { |i| i / 10.0 }
   end
 
   def chars
-    # TODO
+    alphabet = ('a'..'z').to_a.map(&:to_sym)
+    @array.map { |val| alphabet[val - 1] }
   end
 
   def switch
     @newArr = @array.dup
     min = @array.index(@array.min)
     max = @array.index(@array.max)
-    
     @newArr[min], @newArr[max] = @array.max, @array.min
   end
 
   def before_min
-    if @array.index(@array.min) == 0
-      []
-    else
-      @array[@array.index(@array.min) - 1]
-    end
+    @array.select { |val| @array.index(val) < @array.index(@array.min) }
   end
 
   def three_smallest
